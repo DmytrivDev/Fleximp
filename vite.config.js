fbdf8b3156894541d337e0f4020fc92053079b37
Uzurch1k <galcenkovlad7@gmail.com>
2024-05-03 19:53:18 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import vitePluginSass from 'vite-plugin-sass';
 
 export default defineConfig(({ command }) => {
   return {
@@ -25,6 +26,15 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'], vitePluginSass())],
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: `
+            $class: &;
+          `,
+        },
+      },
+    },
   };
 });
