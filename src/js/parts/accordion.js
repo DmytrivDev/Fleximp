beeb473fe9aa731cbac85f0015863ee00b9eda0a
Uzurch1k <galcenkovlad7@gmail.com>
2024-05-04 13:57:00 +0300

    fix

--- src/js/parts/accordion.js
+++ src/js/parts/accordion.js
@@ -1,4 +1,12 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container');
+new Accordion('.accordion-container', {
+  duration: 400,
+  showMultiple: true,
+  onOpen: function (currentElement) {
+    console.log(currentElement);
+  },
+  showMultiple: false,
+  openOnInit: [0],
+});
