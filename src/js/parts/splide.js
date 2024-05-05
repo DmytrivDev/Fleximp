f683bed5753fe6728f5d8e9d5ee91abba0a1c4c4
Uzurch1k <galcenkovlad7@gmail.com>
2024-05-05 17:49:19 +0300

    fix

--- src/js/parts/splide.js
+++ src/js/parts/splide.js
@@ -2,7 +2,7 @@ import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
 
 const instSlider = () => {
-  const slider = document.querySelector('.splide');
+  const slider = document.querySelector('.splide-hero');
 
   if (slider) {
     const options = {
@@ -19,3 +19,31 @@ const instSlider = () => {
   }
 };
 instSlider();
+
+const instSecondSlider = () => {
+  const slider = document.querySelector('.splide-reviews');
+
+  if (slider) {
+    const options = {
+      type: 'loop',
+      speed: 2000,
+      pagination: false,
+      arrows: false,
+      fixedWidth: '48.5625rem',
+      gap: '3.75rem',
+      focus: 'center',
+      updateOnMove: true,
+      perPage: 1,
+      start: 0,
+      // lasses: {
+      //   arrows: 'splide__arrows reviews-arrows',
+      //   arrow: 'splide__arrow reviews-arrow',
+      //   prev: 'splide__arrow--prev reviews-prev',
+      //   next: 'splide__arrow--next reviews-next',
+      // },
+    };
+
+    new Splide(slider, options).mount();
+  }
+};
+instSecondSlider();
