317d359da5fbb6882973f2ed04bc56a9a2ac8b56
DmytrivDev <151439615+DmytrivDev@users.noreply.github.com>
2024-05-07 22:19:25 +0300

    Form

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@ import './parts/accordion';
 import { mobileNav, openNav } from './parts/navigation';
 import './parts/splide';
 import { scrolling } from './parts/scrolling';
+import { formFunc } from './parts/form';
 
 mobileNav();
 openNav();
 scrolling();
+formFunc();
