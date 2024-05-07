be2bc73d0818e5745b3e469d874f3851c82ab73a
Uzurch1k <galcenkovlad7@gmail.com>
2024-05-07 22:59:55 +0300

    fix

--- src/js/parts/accordion.js
+++ src/js/parts/accordion.js
@@ -10,3 +10,16 @@ if (accordionContainer) {
     openOnInit: [0],
   });
 }
+
+const accordionItems = document.querySelectorAll('.services__item');
+
+accordionItems.forEach(item => {
+  item.addEventListener('click', function () {
+    const imgSrc = this.dataset.src;
+    const imgElement = document.querySelector('.services__img-wrapp img');
+
+    if (imgSrc && imgElement) {
+      imgElement.src = imgSrc;
+    }
+  });
+});
