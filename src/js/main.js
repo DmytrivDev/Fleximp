e9e5b260d17e0c6f8e87718d5d202da5f610a99d
Uzurch1k <galcenkovlad7@gmail.com>
2024-05-07 22:29:33 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import './parts/accordion';
 import { mobileNav, openNav } from './parts/navigation';
 import './parts/splide';
 import { scrolling } from './parts/scrolling';
+import './parts/pop-up';
 
 mobileNav();
 openNav();
